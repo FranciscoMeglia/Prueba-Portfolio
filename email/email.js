b3a7d592c98d6e443d49bfa8b3f6f85b68184298
Francisco Meglia <megliafrancisco@gmail.com>
2023-06-26 12:59:10 -0300

    big modified in the code

---
+++ email/email.js
@@ -0,0 +1,46 @@
+const nodemailer = require('nodemailer');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Configura el middleware para analizar los datos enviados en el cuerpo del formulario
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Configura las rutas y el controlador para enviar el correo electrónico
+app.post('/email/enviar-correo', (req, res) => {
+  const { name, email, message } = req.body;
+
+  // Configura el transporte de correo electrónico
+  const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'megliafrancisco@gmail.com',
+      pass: 'scorpion2004ggg'
+    }
+  });
+
+  // Configura los detalles del correo electrónico
+  const mailOptions = {
+    from: email,
+    to: 'megliafrancisco@gmail.com',
+    subject: 'Nuevo mensaje del formulario',
+    text: `Nombre: ${name}\nEmail: ${email}\nMensaje: ${message}`
+  };
+
+  // Envía el correo electrónico
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send('Hubo un error al enviar el mensaje.');
+    } else {
+      console.log('El mensaje ha sido enviado correctamente.');
+      res.send('El mensaje ha sido enviado correctamente.');
+    }
+  });
+});
+
+// Inicia el servidor
+app.listen(3000, () => {
+  console.log('Servidor iniciado en el puerto 3000');
+});
